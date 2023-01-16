b3a372f732b7189082c10e90f0053a985c15db44
MacReiben <macreiben@gmail.com>
2023-01-16 09:36:46 +0100

    working on layout.

--- src/components/RelayComponent.js
+++ src/components/RelayComponent.js
@@ -1,8 +1,52 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+
+/**
+ * 
+    style={[
+        styles.container,
+        {
+            // Try setting `flexDirection` to `"row"`.
+            flexDirection: 'column',
+        },
+    ]}
+ */
 
 const RelayComponent = () => {
-    return <Text>From relay component</Text>
+
+return <View style={[
+    styles.container,
+    {
+        // Try setting `flexDirection` to `"row"`.
+        flexDirection: 'column',
+    },
+]}>
+
+        <View style={[
+            styles.container,
+            {
+                // Try setting `flexDirection` to `"row"`.
+                flexDirection: 'column',
+            }]}>
+            <View><Text>Row 00 - Col00</Text></View>
+            <View><Text>Row 00 - Col01</Text></View>
+            <View><Text>Row 00 - Col02</Text></View>
+        </View>
+        {/* <View><Text>From relay component row 02</Text></View>
+        <View><Text>From relay component row 03</Text></View> */}
+
+    </View>
+
+    
 }
+
+
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+    }
+})
+
 
 export default RelayComponent;
