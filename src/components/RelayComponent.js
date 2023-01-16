ee6d1fab511abe81630e14d6d158d3f562335a3e
MacReiben <macreiben@gmail.com>
2023-01-16 09:49:28 +0100

    relay component with one row.

--- src/components/RelayComponent.js
+++ src/components/RelayComponent.js
@@ -12,41 +12,25 @@ import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
     ]}
  */
 
-const RelayComponent = () => {
-
-return <View style={[
-    styles.container,
-    {
-        // Try setting `flexDirection` to `"row"`.
-        flexDirection: 'column',
-    },
-]}>
-
-        <View style={[
-            styles.container,
-            {
-                // Try setting `flexDirection` to `"row"`.
-                flexDirection: 'column',
-            }]}>
-            <View><Text>Row 00 - Col00</Text></View>
-            <View><Text>Row 00 - Col01</Text></View>
-            <View><Text>Row 00 - Col02</Text></View>
-        </View>
-        {/* <View><Text>From relay component row 02</Text></View>
-        <View><Text>From relay component row 03</Text></View> */}
-
-    </View>
-
-    
-}
-
-
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
+        flex: 1,
     }
 })
-
-
+    
+const RelayComponent = () => {
+    return (
+        <View style={[
+                { 
+                    flexDirection: 'row',
+                    rowGap: 5,
+                    columnGap: 5
+                }
+        ]}>
+            <View><Text>##Row 00 - Col00##</Text></View>
+            <View><Text>##Row 00 - Col01##</Text></View>
+            <View><Text>##Row 00 - Col02##</Text></View>
+        </View>)
+}
 export default RelayComponent;
