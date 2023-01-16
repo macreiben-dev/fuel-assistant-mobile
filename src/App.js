083d1e0d1429e1c129c71f34db81c84ae1f337ba
MacReiben <macreiben@gmail.com>
2023-01-16 09:06:06 +0100

    relay component linked to app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import RelayComponent from './components/RelayComponent'
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Some text up !</Text>
       <StatusBar style="auto" />
+
+      <RelayComponent/>
+
     </View>
   );
 }
---
+++ src/components/RelayComponent.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const RelayComponent = () => {
+    return <Text>From relay component</Text>
+}
+
+export default RelayComponent;
