cdaea9fc44955fe6e841c0a48d7e62c50b5b3f48
MacReiben <macreiben@gmail.com>
2023-01-16 13:43:35 +0100

    row view style factorized.

--- src/components/RelayComponent.js
+++ src/components/RelayComponent.js
@@ -16,21 +16,35 @@ import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+    },
+    rowView: {
+        flexDirection: 'row',
+        rowGap: 5,
+        columnGap: 5
     }
 })
     
 const RelayComponent = () => {
     return (
-        <View style={[
-                { 
-                    flexDirection: 'row',
-                    rowGap: 5,
-                    columnGap: 5
-                }
+        <View  style={[
+            { 
+                flexDirection: 'column',
+                rowGap: 5,
+                columnGap: 5
+            }
         ]}>
-            <View><Text>##Row 00 - Col00##</Text></View>
-            <View><Text>##Row 00 - Col01##</Text></View>
-            <View><Text>##Row 00 - Col02##</Text></View>
-        </View>)
+            <View style={styles.rowView}>
+                <View><Text>##Row 00 - Col00##</Text></View>
+                <View><Text>##Row 00 - Col01##</Text></View>
+                <View><Text>##Row 00 - Col02##</Text></View>
+            </View>
+
+            <View style={styles.rowView}>
+                <View><Text>##Row 01 - Col00##</Text></View>
+                <View><Text>##Row 01 - Col01##</Text></View>
+                <View><Text>##Row 01 - Col02##</Text></View>
+            </View>
+        </View>
+    )
 }
 export default RelayComponent;
