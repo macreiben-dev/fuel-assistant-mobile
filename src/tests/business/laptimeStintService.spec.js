e7413ade955d59e71efea9ae521bf74abb9bf81e
MacReiben <macreiben@gmail.com>
2023-01-18 19:35:01 +0100

    laptimeStintService test added to match expectaction, but can't see the service...

--- src/tests/business/laptimeStintService.spec.js
+++ src/tests/business/laptimeStintService.spec.js
@@ -1,7 +1,8 @@
 const laptimeStintService = (input, props, converter, validator) => {
     
     if(validator(input) === undefined) {
-        props.changeLaptimeMinutes(input)    
+
+        props.changeLaptimeMinutes(converter(input))    
     }
 }
 
@@ -32,6 +33,45 @@ describe('Laptime stint Service', () => {
 
             // ASSERT
             expect(isStoreInvoked).toBe(false)
+        })
+    })
+
+    describe('Given a valid number', () => {
+        test('and the number value is 12 then update store with 13', () => {
+             // ARRANGE
+            let changeLaptimeMinutes = {
+                actualInput: undefined,
+                isStoreInvoked : false
+            }
+
+            var originalProps = {
+                changeLaptimeMinutes:  (input) => {
+                    changeLaptimeMinutes.actualInput = input
+                    changeLaptimeMinutes.isStoreInvoked = true
+                }
+            }
+
+            var validator = (input) => {
+                if(input == 12) {
+                    return undefined
+                }
+
+                return "Some validation error message."
+            }
+
+            var converter = (input) => {
+                if(input == 12) {
+                    return 12
+                }
+                return undefined
+            }
+
+            // ACT
+            laptimeStintService(12, originalProps, converter, validator)
+
+            // ASSERT
+            expect(changeLaptimeMinutes.actualInput).toBe(12)
+            expect(changeLaptimeMinutes.isStoreInvoked).toBe(true)
         })
     })
 })
