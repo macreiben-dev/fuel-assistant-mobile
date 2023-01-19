72bedc4dbc8221513bba59e954471fb273257a39
MacReiben <macreiben@gmail.com>
2023-01-19 09:37:55 +0100

    reorganizing tests files.

--- src/tests/business/stintService.spec.js
+++ src/tests/business/stintService.spec.js
@@ -1,101 +1,130 @@
 import { stintServiceLaptimeMinutes, stintServiceLaptimeSeconds } from "../../business/stintService"
 
 
-describe('Laptime stint Service', () => {
-    describe('Given invalid number for laptimeMinutes', () => {
-        test('Then do not update laptimeMinutes in store.', () => {
+describe('Laptime minutes', () => {
+    test('Given laptime minutes is "hello" then do not update laptimeMinutes in store.', () => {
 
-            // ARRANGE
-            let isStoreInvoked = false
+        // ARRANGE
+        let isStoreInvoked = false
 
-            var originalProps = {
-                changeLaptimeSeconds:  (input) => {
-                    isStoreInvoked = true
-                }
+        var originalProps = {
+            changeLaptimeSeconds:  (input) => {
+                isStoreInvoked = true
+            }
+        }
+
+        var validator = (input) => {
+            if(input == "hello") {
+                return "Some error message"
             }
 
-            var validator = (input) => {
-                if(input == "hello") {
-                    return "Some error message"
-                }
+            return undefined
+        }
+        
+        // ACT
+        stintServiceLaptimeMinutes("hello", originalProps, undefined, validator)
 
-                return undefined
-            }
-            
-            // ACT
-            stintServiceLaptimeMinutes("hello", originalProps, undefined, validator)
-
-            // ASSERT
-            expect(isStoreInvoked).toBe(false)
-        })
+        // ASSERT
+        expect(isStoreInvoked).toBe(false)
     })
 
-    describe('Given a valid number for laptimeMinutes', () => {
-        test('and the number value is 12 then update laptimeMinutes store with 13', () => {
-             // ARRANGE
-            let changeLaptimeSeconds = {
-                actualInput: undefined,
-                isStoreInvoked : false
+    test('Given laptime minutes is 12 then update laptimeMinutes store with 13', () => {
+        // ARRANGE
+        let changeLaptimeMinutes = {
+            actualInput: undefined,
+            isStoreInvoked : false
+        }
+
+        var originalProps = {
+            changeLaptimeMinutes:  (input) => {
+                changeLaptimeMinutes.actualInput = input
+                changeLaptimeMinutes.isStoreInvoked = true
             }
+        }
 
-            var originalProps = {
-                changeLaptimeSeconds:  (input) => {
-                    changeLaptimeSeconds.actualInput = input
-                    changeLaptimeSeconds.isStoreInvoked = true
-                }
-            }
-
-            var validator = (input) => {
-                if(input == 12) {
-                    return undefined
-                }
-
-                return "Some validation error message."
-            }
-
-            var converter = (input) => {
-                if(input == 12) {
-                    return 12
-                }
+        var validator = (input) => {
+            if(input == 12) {
                 return undefined
             }
 
-            // ACT
-            stintServiceLaptimeSeconds(12, originalProps, converter, validator)
+            return "Some validation error message."
+        }
 
-            // ASSERT
-            expect(changeLaptimeSeconds.actualInput).toBe(12)
-            expect(changeLaptimeSeconds.isStoreInvoked).toBe(true)
-        })
+        var converter = (input) => {
+            if(input == 12) {
+                return 12
+            }
+            return undefined
+        }
+
+        // ACT
+        stintServiceLaptimeMinutes(12, originalProps, converter, validator)
+
+        // ASSERT
+        expect(changeLaptimeMinutes.actualInput).toBe(12)
+        expect(changeLaptimeMinutes.isStoreInvoked).toBe(true)
     })
+})
+describe('Laptime seconds', () => {
+    test('Given laptimeSeconds is "hello" then do not update laptimeSeconds in store.', () => {
 
+        // ARRANGE
+        let isStoreInvoked = false
 
-    describe('Given invalid number for laptimeSeconds', () => {
-        test('Then do not update laptimeSeconds in store.', () => {
+        var originalProps = {
+            changeLaptimeSeconds:  (input) => {
+                isStoreInvoked = true
+            }
+        }
 
-            // ARRANGE
-            let isStoreInvoked = false
-
-            var originalProps = {
-                changeLaptimeSeconds:  (input) => {
-                    isStoreInvoked = true
-                }
+        var validator = (input) => {
+            if(input == "hello") {
+                return "Some error message"
             }
 
-            var validator = (input) => {
-                if(input == "hello") {
-                    return "Some error message"
-                }
+            return undefined
+        }
+        
+        // ACT
+        stintServiceLaptimeSeconds("hello", originalProps, undefined, validator)
 
+        // ASSERT
+        expect(isStoreInvoked).toBe(false)
+    })
+    test('Given laptime minutes is 12 then update laptimeMinutes store with 13', () => {
+        // ARRANGE
+        let changeLaptimeSeconds = {
+            actualInput: undefined,
+            isStoreInvoked : false
+        }
+
+        var originalProps = {
+            changeLaptimeSeconds:  (input) => {
+                changeLaptimeSeconds.actualInput = input
+                changeLaptimeSeconds.isStoreInvoked = true
+            }
+        }
+
+        var validator = (input) => {
+            if(input == 12) {
                 return undefined
             }
-            
-            // ACT
-            stintServiceLaptimeSeconds("hello", originalProps, undefined, validator)
 
-            // ASSERT
-            expect(isStoreInvoked).toBe(false)
-        })
+            return "Some validation error message."
+        }
+
+        var converter = (input) => {
+            if(input == 12) {
+                return 12
+            }
+            return undefined
+        }
+
+        // ACT
+        stintServiceLaptimeSeconds(12, originalProps, converter, validator)
+
+        // ASSERT
+        expect(changeLaptimeSeconds.actualInput).toBe(12)
+        expect(changeLaptimeSeconds.isStoreInvoked).toBe(true)
     })
-
 })
