ceb5fe58478265de6938bc8ea6785dfad5c63cc6
MacReiben <macreiben@gmail.com>
2023-01-21 11:59:09 +0100

    set consumption from service is ok.

--- src/tests/business/consumptionService.spec.js
+++ src/tests/business/consumptionService.spec.js
@@ -26,4 +26,39 @@ describe("Consumption service", () => {
     // ASSERT
     expect(isStoreInvoked).toBe(false);
   });
+  test("Given consumption is 12 Then save to store.", () => {
+    // ARRANGE
+    let change = {
+      actualInput: undefined,
+      isStoreInvoked: false,
+    };
+
+    var originalProps = {
+      changeConsumption: (input) => {
+        change.actualInput = input;
+        change.isStoreInvoked = true;
+      },
+    };
+
+    var validator = (input) => {
+      if (input == 12) {
+        return undefined;
+      }
+
+      return "Some validation error message.";
+    };
+
+    var converter = (input) => {
+      if (input == 12) {
+        return 12;
+      }
+      return undefined;
+    };
+
+    // ACT
+    stintSetLiterPerLap(12, originalProps, converter, validator);
+
+    // ASSERT
+    expect(change.isStoreInvoked).toBe(true);
+  });
 });
