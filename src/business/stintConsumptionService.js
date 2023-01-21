e135c7f88105b7c208fbff5ad16e16a949a5e712
MacReiben <macreiben@gmail.com>
2023-01-21 21:52:11 +0100

    service to set the would be stint duration.

--- src/business/stintConsumptionService.js
+++ src/business/stintConsumptionService.js
@@ -4,4 +4,18 @@ const setConsumption = (input, props, converter, validator) => {
   }
 };
 
+const setFuelTankLiter = (input, props, converter, validator) => {
+  if (validator(input) === undefined) {
+    props.changeFuelTankLiter(converter(input));
+  }
+};
+
+const setWouldBeStintDuration = (input, props, converter, validator) => {
+  if (validator(input) === undefined) {
+    props.changeWouldBeStintDuration(converter(input));
+  }
+};
+
 export const stintSetLiterPerLap = setConsumption;
+export const stintSetFuelTankLiter = setFuelTankLiter;
+export const stintSetWouldBeStintDuration = setWouldBeStintDuration;
