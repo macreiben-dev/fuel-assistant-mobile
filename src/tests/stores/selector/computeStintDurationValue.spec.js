711e310d56945d8eb6c1037ec4d218533ae1301d
MacReiben <macreiben@gmail.com>
2023-01-25 18:38:48 +0100

    hour long stint duration properly displayed.

--- src/tests/stores/selector/computeStintDurationValue.spec.js
+++ src/tests/stores/selector/computeStintDurationValue.spec.js
@@ -1,11 +1,11 @@
-import { describe, expect } from "@jest/globals";
+import { describe, expect, test } from "@jest/globals";
 import computeStintDurationValue from "../../../stores/stintdetails/selectors/computeStintDurationValue";
 
 describe("Compute stint duration value", () => {
   describe("compute seconds", () => {
     test("Given decimal second value Then should not return decimal part in duration", () => {
-      const stintPercent = 0.7;
-      const totalStintDurationAsSeconds = 3 * 60;
+      const stintPercent = 1;
+      const totalStintDurationAsSeconds = 2 * 60 + 5;
 
       const actual = computeStintDurationValue(
         totalStintDurationAsSeconds,
@@ -13,6 +13,22 @@ describe("Compute stint duration value", () => {
       );
 
       expect(actual).toBe("00:02:05");
+    });
+  });
+  describe("compute hours", () => {
+    test("Given a 1h15 in as totalStintDuration Then display 01:15:00", () => {
+      const stintPercent = 1;
+      const quarterOfAnHourAsSeconds = 15 * 60;
+      const oneHourAsSeconds = 60 * 60;
+      const totalStintDurationAsSeconds =
+        oneHourAsSeconds + quarterOfAnHourAsSeconds;
+
+      const actual = computeStintDurationValue(
+        totalStintDurationAsSeconds,
+        stintPercent
+      );
+
+      expect(actual).toBe("01:15:00");
     });
   });
 });
