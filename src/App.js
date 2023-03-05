1eda1e81ba1255cec3fcb8b67ef1e2db72cdd3c1
MacReiben <macreiben@gmail.com>
2023-03-05 15:54:12 +0100

    adding navigation.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import { StyleSheet, View } from 'react-native';
-import { Provider, connect } from 'react-redux';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StyleSheet, View } from "react-native";
+import { Provider, connect } from "react-redux";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
+import StintComponent from "./components/StintComponent";
 
-import StintComponent from './components/StintComponent'
-
-import setupStore from './stores/stintdetails/configureStore';
+import setupStore from "./stores/stintdetails/configureStore";
 
 // ==============================================
 
@@ -23,9 +22,9 @@ export default function App() {
   return (
     <Provider store={store}>
       <NavigationContainer>
-      <RootStack.Navigator>
+        <RootStack.Navigator>
           <RootStack.Screen name="StintDetail" component={StintComponent} />
-      </RootStack.Navigator>
+        </RootStack.Navigator>
       </NavigationContainer>
     </Provider>
   );
@@ -34,8 +33,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
