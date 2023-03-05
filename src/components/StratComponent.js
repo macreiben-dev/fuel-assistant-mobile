1b56d16860016fa42ef2c9ab21016d8c32e1396c
MacReiben <macreiben@gmail.com>
2023-03-05 22:08:25 +0100

    experimenting with react native.

--- src/components/StratComponent.js
+++ src/components/StratComponent.js
@@ -1,7 +1,35 @@
 import React from "react";
-import { View, Text, StyleSheet, ScrollView } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  VirtualizedList,
+} from "react-native";
 
 import { Button, DataTable, IconButton, TextInput } from "react-native-paper";
+
+const ItemComponent = (oneItem) => {
+  <View style={{ flex: 1, flexDirection: "row" }}>
+    <View style={{ flexDirection: "column" }}>
+      <View>
+        <Text>Pilot name:</Text>
+        <TextInput placeholder="Pilot name" />
+      </View>
+    </View>
+  </View>;
+};
+
+let items = [
+  {
+    key: 1,
+    pilot: "MacReiben",
+    stintDuration: "00:50:00",
+    pitDuration: "00:00:33",
+    starts: "13:00:00",
+    ends: "13:50:33",
+  },
+];
 
 class StratComponent extends React.Component {
   constructor() {
@@ -14,8 +42,15 @@ class StratComponent extends React.Component {
         <ScrollView>
           <Text>Strat component coming soon !</Text>
 
-          <DataTable>
+          <View style={{ flex: 1, flexDirection: "row" }}>
+            {items.map((item) => (
+              <ItemComponent key={item.id} oneItem={item} />
+            ))}
+          </View>
+
+          {/* <DataTable>
             <DataTable.Header>
+              <DataTable.Title></DataTable.Title>
               <DataTable.Title>Pilot</DataTable.Title>
               <DataTable.Title>Stint Duration</DataTable.Title>
               <DataTable.Title>Pit Duration</DataTable.Title>
@@ -23,17 +58,23 @@ class StratComponent extends React.Component {
               <DataTable.Title>Ends</DataTable.Title>
             </DataTable.Header>
             <DataTable.Row>
-              <DataTable.Cell>Pilot name here</DataTable.Cell>
+              <DataTable.Cell></DataTable.Cell>
+              <DataTable.Cell>CFI</DataTable.Cell>
               <DataTable.Cell>00:00:00</DataTable.Cell>
               <DataTable.Cell>00:00:00</DataTable.Cell>
               <DataTable.Cell>00:00:00</DataTable.Cell>
               <DataTable.Cell>00:00:00</DataTable.Cell>
             </DataTable.Row>
-          </DataTable>
+          </DataTable> */}
         </ScrollView>
 
         <View style={styles.fixedView}>
-          <IconButton icon="plus-circle" />
+          <IconButton
+            icon="plus-circle"
+            size={48}
+            animated={true}
+            onPress={() => console.log("Pressed")}
+          />
         </View>
       </View>
     );
