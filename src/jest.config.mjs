b8c546c378a7c04ae3a61cbc1b523a56c7df18fa
MacReiben <macreiben@gmail.com>
2023-07-06 18:04:52 +0200

    needs to add a webpack alias for a lib.

--- src/jest.config.mjs
+++ src/jest.config.mjs
@@ -75,19 +75,19 @@ export default {
   // ],
 
   // An array of file extensions your modules use
-  // moduleFileExtensions: [
-  //   "js",
-  //   "mjs",
-  //   "cjs",
-  //   "jsx",
-  //   "ts",
-  //   "tsx",
-  //   "json",
-  //   "node"
-  // ],
+  moduleFileExtensions: [
+    "js",
+    "mjs",
+    "cjs",
+    "jsx",
+    "ts",
+    "tsx",
+    "json",
+    "node"
+  ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-  // moduleNameMapper: {},
+  moduleNameMapper: { '^react-native$': 'react-native-web' },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
@@ -166,9 +166,9 @@ export default {
   // ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  // testPathIgnorePatterns: [
-  //   "\\\\node_modules\\\\"
-  // ],
+  testPathIgnorePatterns: [
+    "\\\\node_modules\\\\"
+  ],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
   // testRegex: [],
@@ -183,10 +183,10 @@ export default {
   // transform: undefined,
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-  // transformIgnorePatterns: [
-  //   "\\\\node_modules\\\\",
-  //   "\\.pnp\\.[^\\\\]+$"
-  // ],
+  transformIgnorePatterns: [
+    "/node_modules/(?!(react-native|@react-native|@react-navigation)/)",
+    "/node_modules/@testing-library/react-native"
+  ],
 
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
   // unmockedModulePathPatterns: undefined,
---
+++ src/tests/components/StintComponent.spec.js
@@ -0,0 +1,11 @@
+import { describe, test, expect, beforeEach } from "@jest/globals";
+
+import { render, screen } from '@testing-library/react-native';
+
+import StintComponent from "../../components/StintComponent";
+
+describe('CreateComponent', () => {
+    test('Should build stint component', () => {
+
+    })
+})
