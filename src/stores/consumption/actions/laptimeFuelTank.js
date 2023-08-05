1f57c7660ff0bae54d48df630d9699ab56b615d2
MacReiben <macreiben@gmail.com>
2023-08-05 14:27:51 +0200

    removing old fashioned redux store stuff to use only slices.

--- src/stores/consumption/actions/laptimeFuelTank.js
+++
@@ -1,8 +0,0 @@
-import { FUELTANK_LITER_UPDATE } from "../constants";
-
-export function changeFuelTankLiter(fuelTankLiter) {
-  return {
-    type: FUELTANK_LITER_UPDATE,
-    payload: fuelTankLiter,
-  };
-}
--- src/stores/consumption/actions/laptimeWouldBeStintDurationMinutes.js
+++
@@ -1,8 +0,0 @@
-import { WOULDBE_STINT_DURATION_UPDATE } from "../constants";
-
-export function changeWouldBeStintDuration(durationMinutes) {
-  return {
-    type: WOULDBE_STINT_DURATION_UPDATE,
-    payload: durationMinutes,
-  };
-}
